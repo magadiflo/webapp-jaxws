b9cee466c7e7f05369f0436a47011bfdf0dca070
Martín <magadiflo@gmail.com>
2022-08-19 19:58:19 -0500

    Agregando comentario

--- src/main/java/org/magadiflo/webapp/jaxws/services/ServicioWs.java
+++ src/main/java/org/magadiflo/webapp/jaxws/services/ServicioWs.java
@@ -5,6 +5,10 @@ import org.magadiflo.webapp.jaxws.models.Curso;
 
 import java.util.List;
 
+/**
+ * @WebService, indicará que todos los métodos declarados en esta interfaz
+ * se publicarán, se van a exponer en el servicio web
+ */
 @WebService
 public interface ServicioWs {
 
